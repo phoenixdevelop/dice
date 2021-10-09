9d52d7a9f394b60cfe54bae07e70a8dd6c1db992
phoenixdevelop <78615194+phoenixdevelop@users.noreply.github.com>
2021-10-09 16:38:43 +0300

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,17 @@
+import random 
+dice_current = random.randint(0,7)
+if dice_current = 0:
+    print('Ты проиграл :( Твоё значение равно 0')
+elif dice_current = 1:
+    print('Ты выиграл! Твоё значение равно 1!')
+elif dice_current = 2:
+    print('Ты выиграл! Твоё значение равно 2!')
+elif dice_current = 3:
+    print('Ты выиграл! Твоё значение равно 3!')
+elif dice_current = 4:
+    print('Ты выиграл! Твоё значение равно 4!')
+elif dice_current = 5:
+    print('Ты выиграл! Твоё значение равно 5!')
+elif dice_current = 6:
+    print('Ты выиграл джекпот! Твоё значение равно 6!')
+    
